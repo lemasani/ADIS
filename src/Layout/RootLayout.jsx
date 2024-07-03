c2e4d5bed91c9a6d7219fd4c36b9ac73dd1d90f3
lemasani <brianlemasan@gmail.com>
2024-07-03 23:50:40 +0300

    Remove monitoring link from RootLayout

--- src/Layout/RootLayout.jsx
+++ src/Layout/RootLayout.jsx
@@ -24,7 +24,6 @@ export default function RootLayout({ children }) {
                   </div>
                 )}
               </div>
-              <a href="/monitoring" className="inline-block p-2">Monitoring</a>
               <a href="/logout" className="inline-block p-2">Logout</a>
             </nav>
           </div>
